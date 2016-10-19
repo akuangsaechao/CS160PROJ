1414e3776610558ff09f34506a925657f962f04a
Alvyn Ly <alvyn.ly@gmail.com>
2016-10-19 03:18:25 -0700

    Added dashboard, account, and history

--- Prj1/app.js
+++ Prj1/app.js
@@ -18,6 +18,36 @@ app.config(function($routeProvider) {
 		.when('/signup', {
 				templateUrl: 'signup.html'
 		})
+		.when('/account', {
+				resolve: {
+					"check": function($location, $rootScope) {
+						if(!$rootScope.loggedIn) {
+							$location.path('/');
+						}
+					}
+				},
+				templateUrl: 'account.html'
+		})
+		.when('/request', {
+				resolve: {
+					"check": function($location, $rootScope) {
+						if(!$rootScope.loggedIn) {
+							$location.path('/');
+						}
+					}
+				},
+				templateUrl: 'request.html'
+		})
+		.when('/history', {
+				resolve: {
+					"check": function($location, $rootScope) {
+						if(!$rootScope.loggedIn) {
+							$location.path('/');
+						}
+					}
+				},
+				templateUrl: 'history.html'
+			})
 		.otherwise({
 				redirectTo: '/'
 		});
