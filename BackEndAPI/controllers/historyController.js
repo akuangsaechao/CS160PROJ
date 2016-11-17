231fe64b2b5abf195cebae29bf939500487422b5
akuangsaechao <akuangsaechao@gmail.com>
2016-11-16 17:01:50 -0800

    Added separate history read

--- BackEndAPI/controllers/historyController.js
+++ BackEndAPI/controllers/historyController.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var History = mongoose.model('History');
 
-module.exports.historyRead = function(req, res) {
+module.exports.riderHistoryRead = function(req, res) {
 
   if (!req.payload._id) {
     res.status(401).json({
@@ -9,6 +9,20 @@ module.exports.historyRead = function(req, res) {
     });
   } else {
     History.find({riderName : req.payload.username}, function(err, history) {
+        res.status(200).json(history);
+    });
+  }
+
+};
+
+module.exports.driverHistoryRead = function(req, res) {
+
+  if (!req.payload._id) {
+    res.status(401).json({
+      "message" : "UnauthorizedError: private profile"
+    });
+  } else {
+    History.find({driverName : req.payload.username}, function(err, history) {
         res.status(200).json(history);
     });
   }
